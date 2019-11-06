6924122a0cd14d85218f8ff8efe6b73ee102702d
Greg Nokes <tsykoduk@gmail.com>
2019-11-06 12:07:10 -0800

    update script should trigger a rebuild on Heroku

--- update.sh
+++ update.sh
@@ -6,5 +6,6 @@ bundle update
 git status
 git add .
 git commit -m "updating gems and stuff"
+git commit --allow-empty -m "Trigger rebuild" 
 git push origin master
 heroku logs -t -a mysterious-depths-92606
