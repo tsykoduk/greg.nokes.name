cb74a06f448ddf38418b6f03343eae3e1a95f002
Greg Nokes <greg@nokes.name>
2020-03-22 12:26:48 -0700

    updating gems and stuff

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+myvar=$(git rev-parse --abbrev-ref HEAD)
+git checkout master
 brew upgrade
 heroku update
 sfdx update
@@ -9,4 +11,5 @@ git add .
 git commit -m "updating gems and stuff"
 git commit --allow-empty -m "Trigger rebuild" 
 git push origin master
+git checkout $myvar
 heroku logs -t -a mysterious-depths-92606
