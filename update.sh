7762ba88149a471f8ee9c76f0de1f0720e4a587e
Greg Nokes <greg@nokes.name>
2022-02-21 12:24:05 -0800

    added in for each to handle many remotes with color

--- update.sh
+++ update.sh
@@ -27,7 +27,7 @@ git commit -m "updating gems and stuff"
 git commit --allow-empty -m "Trigger rebuild" 
 
 for fn in `git remote`; do
-	echo "processing remote $fn now"
+	echo "\n \033[34m processing remote $fn now \n\n"
     git push $fn main
 done
 
